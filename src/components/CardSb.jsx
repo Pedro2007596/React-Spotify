2fba1bb5e50257c014d6046052d1de6ed3b4c028
Pedro2007596 <99182973+Pedro2007596@users.noreply.github.com>
2024-09-11 08:42:25 -0300

    React

---
+++ src/components/CardSb.jsx
@@ -0,0 +1,14 @@
+export default function CardSb(){
+
+    return(
+<>
+
+    <div className="bg-red-500 w-32 h-32"></div>
+    <div className="bg-red-500 w-32 h-32"></div>
+    <div className="bg-red-500 w-32 h-32"></div>  
+
+
+</>
+
+    )
+    }
